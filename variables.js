42acf59288ffe498f77df1b11aa5fdc3d7b30c27
RathodAnilT <132031578+RathodAnilT@users.noreply.github.com>
2023-12-21 04:22:06 +0000

    back soon!

---
+++ variables.js
@@ -0,0 +1,10 @@
+const accountId = 142223;
+let accountEmail = "abc@gmail.com";
+var accountPassword = "12345";
+accountCity = "maharastra";
+let accountState;
+
+
+
+console.table([accountId,accountEmail,accountPassword, accountCity, accountState]);
+
