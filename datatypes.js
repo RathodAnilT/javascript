e7fb4c0337c1911078de0a035a4fa249c424991d
RathodAnilT <132031578+RathodAnilT@users.noreply.github.com>
2023-12-21 09:20:49 +0000

    done

---
+++ datatypes.js
@@ -0,0 +1,50 @@
+/*
+JavaScript has 8 Datatypes
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined
+6. Null
+7. Symbol
+8. Object
+
+The Object Datatype
+The object data type can contain:
+
+1. An object
+2. An array
+3. A date
+
+*/
+
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+console.log(length);
+console.log(weight);
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+console.log(color);
+console.log(lastName);
+
+// Booleans
+let x = true;
+let y = false;
+console.log(x);
+console.log(y);
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+console.log(person);
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);
+
+// Date object:
+const date = new Date("2023-12-21");
+console.log(date);
